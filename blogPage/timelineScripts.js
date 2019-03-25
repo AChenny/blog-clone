43dd4be47b60449dd484367d3d2e3972f6ee8d7c
AChenny <anderson_chen97@hotmail.com>
2019-03-25 15:14:02 -0700

    Added bootstrap and its dependencies
    -Moved jquery script to the bottom of the body
    -Added scripts for popper.js, jquery, and bootstrap at the end of the body
    -Linked Bootstrap stylesheets

--- blogPage/timelineScripts.js
+++ blogPage/timelineScripts.js
@@ -1,12 +1,16 @@
 // Jquery functions
-$(document).ready(function() {
 
-});
-
+$(document).keypress(entryKeyPress(event));
 //Regular JS functions
 function addEntry(userName, data) {
   // var blogEntries = document.getElementById("blogEntries");
   $("#blogEntries").append("<div class='entry'>" +
     "<h2>" + userName + "</h2>" +
     "<p>" + data + "<p>" + "</div>" );
+}
+
+function entryKeyPress(e) {
+  if (e.keyCode == 13) {
+    addEntry("Frank", "Keypressed on entry");
+  }
 }
